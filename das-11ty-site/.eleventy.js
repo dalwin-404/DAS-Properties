38dafd408760a455bfb12f5f5abeef02a2ddaaf3
dalwin-404 <ayanboyewinner@gmail.com>
2025-06-28 03:51:23 -0800

    added page

--- das-11ty-site/.eleventy.js
+++ das-11ty-site/.eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("src/assets");
+
     return {
         dir: {
             input: "src",
-            includes: "_includes",  // this should point to where page.njk is
+            includes: "_includes", // add this line
             output: "_site"
         }
     };
