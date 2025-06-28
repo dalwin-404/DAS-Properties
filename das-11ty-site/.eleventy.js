d0402142bd3cd4d96ed4b4fe205a7299336da6f3
dalwin-404 <ayanboyewinner@gmail.com>
2025-06-28 03:58:32 -0800

    fixed

--- das-11ty-site/.eleventy.js
+++ das-11ty-site/.eleventy.js
@@ -1,11 +1,10 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("admin");
-    eleventyConfig.addPassthroughCopy("src/assets");
 
     return {
         dir: {
             input: "src",
-            includes: "_includes", // add this line
+            includes: "_includes",  // ← add this line
             output: "_site"
         }
     };
