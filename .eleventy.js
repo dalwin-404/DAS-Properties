bf8db09ebbd6e339a54213ad3ac395d381237054
dalwin-404 <ayanboyewinner@gmail.com>
2025-07-01 13:05:55 -0800

    updated my eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("static");
+
+    // Add a collection for properties from _data/content/properties (JSON or Markdown)
+    eleventyConfig.addCollection("properties", function (collectionApi) {
+        return collectionApi.getFilteredByGlob("_data/content/properties/*");
+        // Use *.md or *.json if you want to filter by type
+    });
+
     return {
         dir: {
             input: "src",
