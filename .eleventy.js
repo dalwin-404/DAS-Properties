c56bd6b072a1f13a6557f54aa1473e7673e52684
dalwin-404 <ayanboyewinner@gmail.com>
2025-07-02 03:03:40 -0800

    updated file location

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,37 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("admin");
-    eleventyConfig.addPassthroughCopy("static");
+    // Passthrough folders
+    eleventyConfig.addPassthroughCopy("src/admin");
+    eleventyConfig.addPassthroughCopy("src/static");
 
+    // Properties collection
     eleventyConfig.addCollection("properties", function (collectionApi) {
         return collectionApi.getFilteredByGlob("src/properties/*.md");
+    });
+
+    // About collection
+    eleventyConfig.addCollection("about", function (collectionApi) {
+        return collectionApi.getFilteredByGlob("src/about/*.md");
+    });
+
+    // Hero Slides collection
+    eleventyConfig.addCollection("heroSlides", function (collectionApi) {
+        return collectionApi.getFilteredByGlob("src/hero-slides/*.md");
+    });
+
+    // Homepage collection
+    eleventyConfig.addCollection("homepage", function (collectionApi) {
+        return collectionApi.getFilteredByGlob("src/homepage/*.md");
+    });
+
+    // Pages collection
+    eleventyConfig.addCollection("pages", function (collectionApi) {
+        return collectionApi.getFilteredByGlob("src/pages/*.md");
     });
 
     return {
         dir: {
             input: "src",
-            output: "_site"
-        }
+            output: "_site",
+        },
     };
 };
